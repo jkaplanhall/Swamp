39a2894b80c844af9acef51a8273dad41e3a3974
jkaplanhall <36171687+jkaplanhall@users.noreply.github.com>
2021-04-02 21:38:03 -0400

    Initial Commit

---
+++ Assets/SLiiiide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLiiiide : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Vector3 Dir;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Dir * Time.deltaTime;
+    }
+}
